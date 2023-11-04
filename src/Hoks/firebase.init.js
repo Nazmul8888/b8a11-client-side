cf7cc2286ff343bf1405df98a9263b789d4cb3c6
Nazmul8888 <nazmulkhan416@gmail.com>
2023-11-04 19:03:10 +0600

    project all tech stack install complate

---
+++ src/Hoks/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBN1mt6YlZAOyYW1Tu_EAUd-tTUTTRI9d8",
+  authDomain: "assignment-eleven-a2aa1.firebaseapp.com",
+  projectId: "assignment-eleven-a2aa1",
+  storageBucket: "assignment-eleven-a2aa1.appspot.com",
+  messagingSenderId: "194906126859",
+  appId: "1:194906126859:web:74eeff224d64252796650e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
